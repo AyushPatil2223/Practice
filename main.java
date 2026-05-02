c47635bf00e6b328286c0d959f0379d741517b6d
AyushPatil2223 <ayushpatil2229@gmail.com>
2026-05-02 23:01:51 +0530

    input

--- main.java
+++ main.java
@@ -62,3 +62,24 @@ public class Main
 	   
   	}
 }
+
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+     
+       int[]  arr = {6,5,4,3,2,1};
+         int n = arr.length;
+
+         for(int i = 0; i < n ; i++){
+            for(int j = 0 ;  j < n-1-i ; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+         }
+         System.out.print(Arrays.toString(arr));
+
+    }
+}
