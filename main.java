6873768b83471b38e0ea75fd3c0ac750fefd6dd2
AyushPatil2223 <ayushpatil2229@gmail.com>
2026-05-04 22:43:44 +0530

    check in ascending order or not

--- main.java
+++ main.java
@@ -83,3 +83,29 @@ public class Main{
 
     }
 }
+
+
+
+public class Main
+{
+	public static void main(String[] args) {
+		
+		int[] arr = {1,2,3,5,5} ;
+		
+		int n = arr.length;
+		
+		boolean isSort = true;
+		
+		for(int i = 0 ; i < n -1  ; i++){
+		    if(arr[i] != arr[i+1] - 1){
+		        isSort = false;
+		        break;
+		    }
+		        
+		  
+		    
+		}
+		System.out.print(isSort);
+		
+	}
+}
