a32ef64b411ab6763ccfb08312fc0f2014ca581a
AyushPatil2223 <ayushpatil2229@gmail.com>
2026-06-07 13:42:55 +0530

    // Price : 100 80 60 70 60 75 85

--- main.java
+++ main.java
@@ -711,3 +711,32 @@ public class Main
 	     System.out.print(sb.toString());
 	}
 }
+
+// Price : 100 80 60 70 60 75 85
+// Span  :  1  1  1  2  1  4  6
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	   int[] arr = { 100,80,60,70,60,75,85};
+		
+		Stack<Integer> stack = new Stack<>();
+		int[] span = new int[arr.length];
+		for(int i = 0 ; i < arr.length ; i++){
+		while(!stack.isEmpty() && arr[stack.peek()] <= arr[i]) {
+		    stack.pop();
+		}
+		
+		if(stack.isEmpty()){
+		    span[i] = i+  1;
+		}else{
+		    span[i] = i - stack.peek();
+		}
+		
+		stack.push(i);
+		}
+		
+		System.out.print(Arrays.toString(span));
+	}
+}
